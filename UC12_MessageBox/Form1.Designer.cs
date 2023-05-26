0cc29b2fdaca8f294a11eca227be0004f1bbc2ff
LEMUEL CAMPOS MENEZES GOMES <lemuel0072004@gmail.com>
2023-05-25 21:52:31 -0300

    Adicionar arquivos de projeto.

---
+++ UC12_MessageBox/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace UC12_MessageBox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonMB = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonMB
+            // 
+            this.buttonMB.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonMB.Location = new System.Drawing.Point(145, 95);
+            this.buttonMB.Name = "buttonMB";
+            this.buttonMB.Size = new System.Drawing.Size(173, 84);
+            this.buttonMB.TabIndex = 0;
+            this.buttonMB.Text = "MessageBox";
+            this.buttonMB.UseVisualStyleBackColor = true;
+            this.buttonMB.Click += new System.EventHandler(this.buttonMB_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(458, 297);
+            this.Controls.Add(this.buttonMB);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonMB;
+    }
+}
+
---
+++ UC12_MessageBox/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UC12_MessageBox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonMB_Click(object sender, EventArgs e)
+        {
+            DialogResult decisao = MessageBox.Show("Deseja Continuar?", "Opção", MessageBoxButtons.YesNo);
+
+            if (decisao == DialogResult.Yes)
+            {
+                MessageBox.Show("Você clicou em Sim!");
+            }
+            else
+            {
+                MessageBox.Show("Você clicou em Não!");
+            }
+        }
+    }
+}
